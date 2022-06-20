36c8a66c38c6ec42808d257f41911a116272872c
tylerhill122 <98857284+tylerhill122@users.noreply.github.com>
2022-06-20 11:30:19 -0400

    populating the dropdown menu, fat arrow function now

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,11 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-d3.json(url).then(function(data){ 
-    console.log(data)
+d3.json(url).then((data) => { 
+    console.log(data);
     let names = data.names;
-    console.log(names)
+    names.forEach((name) => {
+        d3.select("#selDataset").append("option").text(name);
+    })
 });
 
 d3.selectAll("#selDataset").on("change", getData);
